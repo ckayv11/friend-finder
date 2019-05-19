07fc1b4c212c447318acd830a9623382bcfe5f63
CKayV11 <11303975+ckayv11@users.noreply.github.com>
2019-05-19 19:40:17 -0400

    added friends data to friends.js & added node_modules/ to .gitignore file

---
+++ FriendFinder/app/data/friends.js
@@ -0,0 +1,55 @@
+var friends = [
+    {
+        name: "Jim Halpert",
+        photo: "https://pbs.twimg.com/profile_images/3171824697/ef75d90df2e65ce326acf30262df5918.jpeg",
+        scores: [5, 1, 4, 4, 5, 1, 2, 5, 4, 1]
+    },
+    {
+        name: "Dwight Schrute",
+        photo: "https://66.media.tumblr.com/eca4448ac34a732443b9495be9400df9/tumblr_o8t561FfNs1ut6yz1o1_1280.jpg",
+        scores: [1, 3, 4, 5, 2, 2, 3, 4, 1, 5]
+    },
+    {
+        name: "Pam Beesley",
+        photo: "https://chumley.barstoolsports.com/wp-content/uploads/2018/10/25/Screen-Shot-2018-10-25-at-12.15.41-PM.png",
+        scores: [1, 4, 5, 2, 1, 5, 4, 4, 1, 5]
+    },
+    {
+        name: "Kelly Kapoor",
+        photo: "https://media1.popsugar-assets.com/files/thumbor/eXDFklp6xdWXLyvn9BcH5p-b_rk/fit-in/2048xorig/filters:format_auto-!!-:strip_icc-!!-/2016/06/21/951/n/1922283/05e3e789_edit_img_image_41730517_1466542783_kelly-th/i/Kelly-Kapoor-GIFs-From-Office.jpg",
+        scores: [4, 2, 5, 5, 4, 1, 1, 4, 5, 2]
+    },
+    {
+        name: "Michael Scott",
+        photo: "https://boston.citymomsblog.com/wp-content/uploads/sites/55/2018/02/does-the-office-hold-up-gq.jpg",
+        scores: [3, 5, 4, 1, 1, 2, 3, 5, 4, 1]
+    },
+    {
+        name: "Stanley Hudson",
+        photo: "https://www.datocms-assets.com/151/1471782468-stanley.png?w=500&fm=jpg",
+        scores: [1, 4, 5, 2, 2, 5, 5, 4, 1, 2]
+    },
+    {
+        name: "Oscar Martinez",
+        photo: "https://pbs.twimg.com/profile_images/956263585251905536/HI07Q-EL_400x400.jpg",
+        scores: [2, 1, 4, 5, 5, 2, 2, 5, 4, 1]
+    },
+    {
+        name: "Phyllis Vance",
+        photo: "https://66.media.tumblr.com/f6c509e56f5a5fae9819d6b5acffe925/tumblr_oadni31rnM1s6205uo1_1280.jpg",
+        scores: [3, 3, 4, 4, 5, 5, 1, 1, 2, 2]
+    },
+    {
+        name: "Darryl Philbin",
+        photo: "https://costumewall.com/wp-content/uploads/2017/04/darryl-philbin.jpg",
+        scores: [2, 2, 1, 1, 4, 4, 5, 5, 3, 3]
+    },
+    {
+        name: "Erin Hannon",
+        photo: "http://brainsandcareers.com/wp-content/uploads/2018/08/Ellie-Kemper-Erin-Hannon-US-Office.jpg",
+        scores: [2, 4, 1, 3, 5, 4, 5, 2, 1, 1]
+    },
+
+]
+
+module.exports = friends;
